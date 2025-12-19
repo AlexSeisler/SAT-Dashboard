cd2644b80780191d3e6af55d2b57f9c6c572eff5
AlexSeisler <alex@acsresultsai.net>
2025-12-19 07:23:44 -0500

    feat: add logging to static file serving

--- server/static.ts
+++ server/static.ts
@@ -4,6 +4,7 @@ import path from "path";
 
 export function serveStatic(app: Express) {
   const distPath = path.resolve(__dirname, "public");
+  console.log(`[serveStatic] Serving static files from: ${distPath}`);
   if (!fs.existsSync(distPath)) {
     throw new Error(
       `Could not find the build directory: ${distPath}, make sure to build the client first`,
@@ -13,7 +14,8 @@ export function serveStatic(app: Express) {
   app.use(express.static(distPath));
 
   // fall through to index.html if the file doesn't exist
-  app.use("*", (_req, res) => {
+  app.use("*", (req, res) => {
+    console.log(`[serveStatic] Catch-all for ${req.originalUrl}, sending index.html`);
     res.sendFile(path.resolve(distPath, "index.html"));
   });
 }
